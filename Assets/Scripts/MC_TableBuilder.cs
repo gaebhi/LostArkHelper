4b6fdfd2ef653887d0f16b335b325e5bf92c6f40
gaebhi <kdkim89@gmail.com>
2019-07-10 21:19:08 +0900

    코드 정리

---
+++ Assets/Scripts/MC_TableBuilder.cs
@@ -0,0 +1,538 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MC_TableBuilder : MonoBehaviour
+{
+    public static MC_TableBuilder Instance;
+
+    //CotinentList
+    [HideInInspector]
+    public List<string> are = new List<string>();
+    [HideInInspector]
+    public List<string> udia = new List<string>();
+    [HideInInspector]
+    public List<string> luteranWest = new List<string>();
+    [HideInInspector]
+    public List<string> luteranEast = new List<string>();
+    [HideInInspector]
+    public List<string> anits = new List<string>();
+    [HideInInspector]
+    public List<string> arde = new List<string>();
+    [HideInInspector]
+    public List<string> sh = new List<string>();
+    [HideInInspector]
+    public List<string> bernNorth = new List<string>();
+    [HideInInspector]
+    public List<string> greatVoyage = new List<string>();
+
+    //map name
+    public string Ar_leon
+    {
+        get
+        {
+            return ar_leon;
+        }
+    }
+
+    public string Ar_log
+    {
+        get
+        {
+            return ar_log;
+        }
+    }
+
+    public string Ar_ange
+    {
+        get
+        {
+            return ar_ange;
+        }
+    }
+
+    public string Ar_guk
+    {
+        get
+        {
+            return ar_guk;
+        }
+    }
+
+    public string Udia_sa
+    {
+        get
+        {
+            return udia_sa;
+        }
+    }
+
+    public string Udia_oz
+    {
+        get
+        {
+            return udia_oz;
+        }
+    }
+
+    public string Lw_guk
+    {
+        get
+        {
+            return lw_guk;
+        }
+    }
+
+    public string Lw_lake
+    {
+        get
+        {
+            return lw_lake;
+        }
+    }
+
+    public string Lw_mad
+    {
+        get
+        {
+            return lw_mad;
+        }
+    }
+
+    public string Lw_bil
+    {
+        get
+        {
+            return lw_bil;
+        }
+    }
+
+    public string Lw_zago
+    {
+        get
+        {
+            return lw_zago;
+        }
+    }
+
+    public string Le_gal
+    {
+        get
+        {
+            return le_gal;
+        }
+    }
+
+    public string Le_dio
+    {
+        get
+        {
+            return le_dio;
+        }
+    }
+
+    public string Le_lia
+    {
+        get
+        {
+            return le_lia;
+        }
+    }
+
+    public string Le_lu
+    {
+        get
+        {
+            return le_lu;
+        }
+    }
+
+    public string Le_bae
+    {
+        get
+        {
+            return le_bae;
+        }
+    }
+
+    public string Le_bo
+    {
+        get
+        {
+            return le_bo;
+        }
+    }
+
+    public string Le_cro
+    {
+        get
+        {
+            return le_cro;
+        }
+    }
+
+    public string Le_hae
+    {
+        get
+        {
+            return le_hae;
+        }
+    }
+
+    public string Le_hk
+    {
+        get
+        {
+            return le_hk;
+        }
+    }
+
+    public string An_mirror
+    {
+        get
+        {
+            return an_mirror;
+        }
+    }
+
+    public string An_del
+    {
+        get
+        {
+            return an_del;
+        }
+    }
+
+    public string An_dung
+    {
+        get
+        {
+            return an_dung;
+        }
+    }
+
+    public string An_sori
+    {
+        get
+        {
+            return an_sori;
+        }
+    }
+
+    public string An_hang
+    {
+        get
+        {
+            return an_hang;
+        }
+    }
+
+    public string An_hwang
+    {
+        get
+        {
+            return an_hwang;
+        }
+    }
+
+    public string Ard_gal
+    {
+        get
+        {
+            return ard_gal;
+        }
+    }
+
+    public string Ard_ne
+    {
+        get
+        {
+            return ard_ne;
+        }
+    }
+
+    public string Ard_lise
+    {
+        get
+        {
+            return ard_lise;
+        }
+    }
+
+    public string Ard_me
+    {
+        get
+        {
+            return ard_me;
+        }
+    }
+
+    public string Ard_wind
+    {
+        get
+        {
+            return ard_wind;
+        }
+    }
+
+    public string Ard_sh
+    {
+        get
+        {
+            return ard_sh;
+        }
+    }
+
+    public string Ard_to
+    {
+        get
+        {
+            return ard_to;
+        }
+    }
+
+    public string Ard_red
+    {
+        get
+        {
+            return ard_red;
+        }
+    }
+
+    public string Bn_la
+    {
+        get
+        {
+            return bn_la;
+        }
+    }
+
+    public string Bn_val
+    {
+        get
+        {
+            return bn_val;
+        }
+    }
+
+    public string Bn_be
+    {
+        get
+        {
+            return bn_be;
+        }
+    }
+
+    public string Bn_bc
+    {
+        get
+        {
+            return bn_bc;
+        }
+    }
+
+    public string Bn_cro
+    {
+        get
+        {
+            return bn_cro;
+        }
+    }
+
+    public string Bn_par
+    {
+        get
+        {
+            return bn_par;
+        }
+    }
+
+    public string Bn_pes
+    {
+        get
+        {
+            return bn_pes;
+        }
+    }
+
+    public string Sh_li
+    {
+        get
+        {
+            return sh_li;
+        }
+    }
+
+    public string Sh_mu
+    {
+        get
+        {
+            return sh_mu;
+        }
+    }
+
+    public string Sh_su
+    {
+        get
+        {
+            return sh_su;
+        }
+    }
+
+    public string Sh_freeze
+    {
+        get
+        {
+            return sh_freeze;
+        }
+    }
+
+    public string Sh_ice
+    {
+        get
+        {
+            return sh_ice;
+        }
+    }
+
+    public string Sh_kal
+    {
+        get
+        {
+            return sh_kal;
+        }
+    }
+
+    //map name
+
+    //아르테미스
+    private const string ar_leon = "ar_leon";
+    private const string ar_log = "ar_log";
+    private const string ar_ange = "ar_ange";
+    private const string ar_guk = "ar_guk";
+
+    //유디아
+    private const string udia_sa = "udia_sa";
+    private const string udia_oz = "udia_oz";
+
+    //루테란서부
+    private const string lw_guk = "lw_guk";
+    private const string lw_lake = "lw_lake";
+    private const string lw_mad = "lw_mad";
+    private const string lw_bil = "lw_bil";
+    private const string lw_zago = "lw_zago";
+
+    //루테란동부
+    private const string le_gal = "le_gal";
+    private const string le_dio = "le_dio";
+    private const string le_lia = "le_lia";
+    private const string le_lu = "le_lu";
+    private const string le_bae = "le_bae";
+    private const string le_bo = "le_bo";
+    private const string le_cro = "le_cro";
+    private const string le_hae = "le_hae";
+    private const string le_hk = "le_hk";
+
+    //애니츠
+    private const string an_mirror = "an_mirror";
+    private const string an_del = "an_del";
+    private const string an_dung = "an_dung";
+    private const string an_sori = "an_sori";
+    private const string an_hang = "an_hang";
+    private const string an_hwang = "an_hwang";
+
+    //아르데타인
+    private const string ard_gal = "ard_gal";
+    private const string ard_ne = "ard_ne";
+    private const string ard_lise = "ard_lse";
+    private const string ard_me = "ard_me";
+    private const string ard_wind = "ard_wind";
+    private const string ard_sh = "ard_sh";
+    private const string ard_to = "ard_to";
+    private const string ard_red = "ard_red";
+
+    //베른북부
+    private const string bn_la = "bn_la";
+    private const string bn_val = "bn_val";
+    private const string bn_be = "bn_be";
+    private const string bn_bc = "bn_bc";
+    private const string bn_cro = "bn_cro";
+    private const string bn_par = "bn_par";
+    private const string bn_pes = "bn_pes";
+
+    //슈샤이어
+    private const string sh_li = "sh_li";
+    private const string sh_mu = "sh_mu";
+    private const string sh_su = "sh_su";
+    private const string sh_freeze = "sh_freeze";
+    private const string sh_ice = "sh_ice";
+    private const string sh_kal = "sh_kal";
+
+    private void Awake()
+    {
+        Instance = this;
+
+        //아르테미스
+        are.Add("레온하트");
+        are.Add("로그힐");
+        are.Add("안게모스산기슭");
+        are.Add("국경지대");
+
+        //유디아
+        udia.Add("살란드구릉지");
+        udia.Add("오즈혼구릉지");
+
+        //루테란서부
+        luteranWest.Add("격전의평야");
+        luteranWest.Add("레이크바");
+        luteranWest.Add("메드리닉수도원");
+        luteranWest.Add("빌브린숲");
+        luteranWest.Add("자고라스산");
+
+        //루테란동부
+        luteranEast.Add("갈기파도항구");
+        luteranEast.Add("디오리카평원");
+        luteranEast.Add("라이아단구");
+        luteranEast.Add("루테란성");
+        luteranEast.Add("배꽃나무자생지");
+        luteranEast.Add("보레야영지");
+        luteranEast.Add("크로커니스해변");
+        luteranEast.Add("헤무리언덕");
+        luteranEast.Add("흑장미교회당");
+
+        //애니츠
+        anits.Add("거울계곡");
+        anits.Add("델파이현");
+        anits.Add("등나무언덕");
+        anits.Add("소리의숲");
+        anits.Add("항구도시창천");
+        anits.Add("황혼의연무");
+
+        //아르데타인
+        arde.Add("갈라진땅");
+        arde.Add("네벨호른");
+        arde.Add("리제폭포");
+        arde.Add("메마른통로");
+        arde.Add("바람결구릉지");
+        arde.Add("슈테른");
+        arde.Add("토트리치");
+        arde.Add("붉은모래사막");
+
+        //베른북부
+        bernNorth.Add("라니아마을");
+        bernNorth.Add("발란카르산맥");
+        bernNorth.Add("베르닐삼림");
+        bernNorth.Add("베른성");
+        bernNorth.Add("크로나항구");
+        bernNorth.Add("파르나숲");
+        bernNorth.Add("페스나르고원");
+
+        //슈샤이어
+        sh.Add("리겐스마을");
+        sh.Add("머무른시간의호수");
+        sh.Add("서리감옥고원");
+        sh.Add("얼어붙은바다");
+        sh.Add("얼음나비절벽");
+        sh.Add("칼날바람언덕");
+    }
+}
---
+++ Assets/Scripts/MC_UIRoot.cs
@@ -0,0 +1,275 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MC_UIRoot : MonoBehaviour
+{
+    [SerializeField]
+    private UISprite spriteMap;
+    [SerializeField]
+    private UIPopupList continentList;
+    [SerializeField]
+    private UILabel labelContinet;
+    [SerializeField]
+    private UIPopupList areaList;
+    [SerializeField]
+    private UILabel labelArea;
+
+    public static int Width { get; set; }
+    public static int Height { get; set; }
+
+    void Awake()
+    {
+        float screenRatio = (float)Screen.currentResolution.width / (float)Screen.currentResolution.height;
+
+        Height = Mathf.FloorToInt(Screen.currentResolution.height / 1.2f);
+        Width = Mathf.FloorToInt(Height * screenRatio);
+
+        float spriteRatio = (float)Width / (float)Height;
+
+        int spriteHeight = Mathf.FloorToInt(Height / 1.2f);
+        int spriteWidth = Mathf.FloorToInt(spriteHeight * spriteRatio);
+
+        spriteMap.width = spriteWidth;
+        spriteMap.height = spriteHeight;
+    }
+    public void SetContinentSelection()
+    {
+        if (continentList != null)
+        {
+            labelContinet.text = continentList.value;
+            areaList.Clear();
+            switch (continentList.value)
+            {
+                case "아르테미스":
+                    foreach (var item in MC_TableBuilder.Instance.are)
+                    {
+                        areaList.AddItem(item);
+                    }
+                    break;
+                case "유디아":
+                    foreach (var item in MC_TableBuilder.Instance.udia)
+                    {
+                        areaList.AddItem(item);
+                    }
+                    break;
+                case "루테란서부":
+                    foreach (var item in MC_TableBuilder.Instance.luteranWest)
+                    {
+                        areaList.AddItem(item);
+                    }
+                    break;
+                case "루테란동부":
+                    foreach (var item in MC_TableBuilder.Instance.luteranEast)
+                    {
+                        areaList.AddItem(item);
+                    }
+                    break;
+                case "애니츠":
+                    foreach (var item in MC_TableBuilder.Instance.anits)
+                    {
+                        areaList.AddItem(item);
+                    }
+                    break;
+                case "아르데타인":
+                    foreach (var item in MC_TableBuilder.Instance.arde)
+                        areaList.AddItem(item);
+                    break;
+                case "슈샤이어":
+                    foreach (var item in MC_TableBuilder.Instance.sh)
+                    {
+                        areaList.AddItem(item);
+                    }
+                    break;
+                case "베른북부":
+                    foreach (var item in MC_TableBuilder.Instance.bernNorth)
+                    {
+                        areaList.AddItem(item);
+                    }
+                    break;
+                case "대항해":
+                    foreach (var item in MC_TableBuilder.Instance.greatVoyage)
+                    {
+                        areaList.AddItem(item);
+                    }
+                    break;
+                default:
+                    foreach (var item in MC_TableBuilder.Instance.are)
+                    {
+                        areaList.AddItem(item);
+                    }
+                    break;
+            }
+        }
+    }
+
+    public void SetAreaSelection()
+    {
+        if (areaList != null)
+        {
+            labelArea.text = areaList.value;
+            switch (areaList.value)
+            {
+                //아르테미스
+                case "레온하트":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Ar_leon;
+                    break;
+                case "로그힐":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Ar_log;
+                    break;
+                case "안게모스산기슭":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Ar_ange;
+                    break;
+                case "국경지대":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Ar_guk;
+                    break;
+
+                //유디아
+                case "살란드구릉지":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Udia_sa;
+                    break;
+                case "오즈혼구릉지":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Udia_oz;
+                    break;
+
+                //루테란서부
+                case "격전의평야":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Lw_guk;
+                    break;
+                case "레이크바":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Lw_lake;
+                    break;
+                case "메드리닉수도원":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Lw_mad;
+                    break;
+                case "빌브린숲":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Lw_bil;
+                    break;
+                case "자고라스산":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Lw_zago;
+                    break;
+
+                //루테란동부
+                case "갈기파도항구":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Le_gal;
+                    break;
+                case "디오리카평원":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Le_dio;
+                    break;
+                case "라이아단구":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Le_lia;
+                    break;
+                case "루테란성":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Le_lu;
+                    break;
+                case "배꽃나무자생지":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Le_bae;
+                    break;
+                case "보레야영지":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Le_bo;
+                    break;
+                case "크로커니스해변":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Le_cro;
+                    break;
+                case "헤무리언덕":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Le_hae;
+                    break;
+                case "흑장미교회당":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Le_hk;
+                    break;
+
+                //애니츠
+                case "거울계곡":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.An_mirror;
+                    break;
+                case "델파이현":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.An_del;
+                    break;
+                case "등나무언덕":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.An_dung;
+                    break;
+                case "소리의숲":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.An_sori;
+                    break;
+                case "항구도시창천":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.An_hang;
+                    break;
+                case "황혼의연무":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.An_hwang;
+                    break;
+
+                //아르데타인
+                case "갈라진땅":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Ard_gal;
+                    break;
+                case "네벨호른":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Ard_ne;
+                    break;
+                case "리제폭포":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Ard_lise;
+                    break;
+                case "메마른통로":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Ard_me;
+                    break;
+                case "바람결구릉지":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Ard_wind;
+                    break;
+                case "슈테른":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Ard_sh;
+                    break;
+                case "토트리치":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Ard_to;
+                    break;
+                case "붉은모래사막":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Ard_red;
+                    break;
+
+                //베른북부
+                case "라니아마을":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Bn_la;
+                    break;
+                case "발란카르산맥":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Bn_val;
+                    break;
+                case "베르닐삼림":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Bn_be;
+                    break;
+                case "베른성":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Bn_bc;
+                    break;
+                case "크로나항구":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Bn_cro;
+                    break;
+                case "파르나숲":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Bn_par;
+                    break;
+                case "페스나르고원":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Bn_pes;
+                    break;
+
+                //슈샤이어
+                case "리겐스마을":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Sh_li;
+                    break;
+                case "머무른시간의호수":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Sh_mu;
+                    break;
+                case "서리감옥고원":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Sh_su;
+                    break;
+                case "얼어붙은바다":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Sh_freeze;
+                    break;
+                case "얼음나비절벽":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Sh_ice;
+                    break;
+                case "칼날바람언덕":
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Sh_kal;
+                    break;
+                default:
+                    spriteMap.spriteName = MC_TableBuilder.Instance.Ar_leon;
+                    break;
+            }
+        }
+    }
+}
